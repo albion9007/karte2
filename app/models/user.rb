e441a6da0009c32142b20996e83ce9abfbbdddd6
albion9007 <68537855+albion9007@users.noreply.github.com>
2020-09-02 14:39:56 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, uniqueness: { case_sensitive: true }
+  end
 end
