413d27bb96ebf2b25a8e28cc55f3c81d4b184a72
albion9007 <68537855+albion9007@users.noreply.github.com>
2020-09-02 21:14:22 +0900

    userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :nickname, uniqueness: { case_sensitive: true }
+    validates :nickname, uniqueness: { case_sensitive: true }, length: { maximum: 6 }
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   end
 end
